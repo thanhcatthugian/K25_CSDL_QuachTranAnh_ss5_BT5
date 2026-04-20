33ce9ebe5d19cc8bfd3d6e14d3660e5d8dad4aff
Quach Tran Anh <quachtrananh1609@gmail.com>
2026-04-21 00:12:35 +0700

    Add files via upload

--- KS25_CSDL_QuachTranAnh_SS5_BT5.sql
+++ KS25_CSDL_QuachTranAnh_SS5_BT5.sql
@@ -4,7 +4,23 @@ not null hoặc cài đặt default là 0
 */
 create database ss5bai5;
 use ss5bai5;
-select Tên_Khách_Hàng,
+create table Users(
+customerName varchar(50) not null,
+id int primary key auto_increment,
+total_orders int not null
+);
+INSERT INTO Users (customerName, total_orders) VALUES
+('Nguyen Van A', 600),  
+('Tran Thi B', 350),     
+('Le Van C', 45),       
+('Pham Van D', 550),    
+('Hoang Thi E', 120), 
+('Do Van F', 80),   
+('Vu Thi G', 1000),      
+('Ngo Van H', 499),   
+('Dang Thi I', 10),  
+('Bui Van J', 200);      
+select customerName,
 case
 when total_orders > 500 then 'Kim Cương'
 when total_orders between 100 and 499 then 'Vàng'
